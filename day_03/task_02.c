03ba2d77f027256749e90d0acb8f3448310a6fcc
yeti675 <yeti91750@gmail.com>
2018-10-16 12:07:59 +0200

    maj

---
+++ day_03/task_02.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void my_putchar(char);
+int my_print_revalpha(void);
+
+int main(void) {
+   my_print_revalpha();
+   
+    return(0);
+}
+
+void my_putchar(char c ) {   
+
+    write(1, &c, 1);
+}
+
+int my_print_revalpha(void){
+
+    char letter;
+       
+    letter = 'z';
+
+    while (letter >= 'a') {
+
+        my_putchar(letter);
+        letter--;  
+    }
+    my_putchar('\n');
+}
